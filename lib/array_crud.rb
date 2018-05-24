956b428fa7a24f3bccd93499bd81f81a857b40e7
Allegra <aukseallegra@gmail.com>
2018-05-24 22:06:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = ["data1", "data2"]
+  my_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
