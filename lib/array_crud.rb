28f7adb56966b64c73ee6420cbc0dcfb242a63da
Allegra <aukseallegra@gmail.com>
2018-05-24 22:06:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["data1", "data2", "data3"]
+  my_array = ["data1", "data2", "data3", "data4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = ["data1", "data2"]
+  my_array << element 
 end
 
 def add_element_to_start_of_array(array, element)
